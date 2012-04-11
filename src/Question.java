48238b0d723f43101ca2ad553c746386517e87d6
Jakob Olandt <jolandt@asu.edu>
2012-04-10 18:26:14 -0700

    removed pointless comment

--- src/Question.java
+++ src/Question.java
@@ -18,7 +18,6 @@ public class Question {
 	// 										constructors
 	//--------------------------------------------------------------------------------------------
 	
-	//pointless comment
 	public Question(){
 		questionBody = "";
 		correctAnswer = 1;
