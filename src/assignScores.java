0232363378ba55a22eda754f61c4cd3cd688be6f
Jakob Olandt <jolandt@asu.edu>
2012-04-24 12:03:56 -0700

    now has a refresh button

--- src/assignScores.java
+++ src/assignScores.java
@@ -1,13 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.event.*;
 import java.util.*;
 
 public class assignScores extends JPanel
 {
     private JList assign;
-    private JButton view;
+    private JButton view, refresh;
+    private JPanel box;
     private Vector<Assignment> assignments;
     private responseStats stats;
     private Instructor i;
@@ -18,13 +20,21 @@ public class assignScores extends JPanel
     	setLayout(new GridLayout(2,1));
         assignments = i.getAList();
         
+        box = new JPanel();
+        box.setLayout(new GridLayout(1,2));
+        
         view = new JButton("view");
         view.addActionListener(new view());
+        
+        refresh = new JButton("refresh");
+        refresh.addActionListener(new refresh());
+        box.add(view);
+        box.add(refresh);
         
         assign = new JList(assignments);
         
         add(assign);
-        add(view);
+        add(box);
         
         setVisible(true);    
 
@@ -34,11 +44,23 @@ public class assignScores extends JPanel
     {
          public void actionPerformed(ActionEvent e)
          {
+        	 
         	 Assignment temp = assignments.get(assign.getSelectedIndex());
         	 System.out.println(temp);
             stats = new responseStats(temp, i);
             stats.setVisible(true);
+            
          }
+    }
+    
+    private class refresh implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			assign.setListData(assignments);
+			
+		}
+    	
     }
     
 }
